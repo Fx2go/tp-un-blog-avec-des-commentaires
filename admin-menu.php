e3a4b6725ce6b9aebc9622c08540fd1f88c8c1aa
François-Xavier GODIN <fx.godin@gmail.com>
2018-01-16 18:01:54 +0100

    Modif url des billets (param"billet" devient "id")

--- admin-menu.php
+++ admin-menu.php
@@ -1,7 +1,8 @@
 	<div class="edition">
 		<ul class="edition">
 			<li><a href="./admin/ajouter.php" title="Ajouter un nouveau billet"><i class="fa fa-plus-square" aria-hidden="true"></i></a></li>
-			<li><a href="./admin/modifier.php/?billet=<?php echo $id_billet?>" title="Modifier ce billet"><i class="fa fa-pencil" aria-hidden="true"></i></a></li>
-			<li><a href="./admin/supprimer.php" onclick="return confirm('&Ecirc;tes-vous sûr de vouloir supprimer ce magnifique Billet ? (Il sera perdu à jamais) ')" title="Supprimer ce billet !"><i class="fa fa-trash" aria-hidden="true"></i></a></li>
+			<li><a href="./admin/modifier.php/?id=<?php echo $id_billet?>" title="Modifier ce billet"><i class="fa fa-pencil" aria-hidden="true"></i></a></li>
+			<li><a href="./admin/supprimer.php/?id=<?php echo $id_billet?>" onclick="return confirm('&Ecirc;tes-vous sûr de vouloir supprimer ce magnifique Billet ? (Il sera perdu à jamais) ')" title="Supprimer ce billet !"><i class="fa fa-trash" aria-hidden="true"></i></a></li>
 		</ul>
 	</div>
+
